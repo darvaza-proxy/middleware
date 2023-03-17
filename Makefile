b984337bae5a09c17a68b28092897997f6f9c96f
Alejandro Mery <amery@jpi.io>
2023-03-17 16:28:34 +0000

    build-sys: import Makefile and submodule support
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ Makefile
@@ -0,0 +1,57 @@
+.PHONY: all clean generate tidy fmt
+.PHONY: FORCE
+
+GO ?= go
+GOFMT ?= gofmt
+GOFMT_FLAGS = -w -l -s
+GOGENERATE_FLAGS = -v
+
+GOPATH ?= $(shell $(GO) env GOPATH)
+GOBIN ?= $(GOPATH)/bin
+
+TOOLSDIR := $(CURDIR)/internal/tools
+TMPDIR ?= $(CURDIR)/.tmp
+
+REVIVE ?= $(GOBIN)/revive
+REVIVE_CONF ?= $(TOOLSDIR)/revive.toml
+REVIVE_RUN_ARGS ?= -config $(REVIVE_CONF) -formatter friendly
+
+REVIVE_INSTALL_URL ?= github.com/mgechev/revive
+
+GO_INSTALL_URLS = \
+	$(REVIVE_INSTALL_URL) \
+
+GO_BUILD = $(GO) build -v
+GO_BUILD_CMD = $(GO_BUILD) -o $(TMPDIR)
+
+V = 0
+Q = $(if $(filter 1,$V),,@)
+M = $(shell if [ "$$(tput colors 2> /dev/null || echo 0)" -ge 8 ]; then printf "\033[34;1m▶\033[0m"; else printf "▶"; fi)
+
+all: get generate tidy build
+
+clean: ; $(info $(M) cleaning…)
+	rm -rf $(TMPDIR)
+
+$(TMPDIR)/index: $(TOOLSDIR)/gen_index.sh Makefile FORCE ; $(info $(M) generating index…)
+	$Q mkdir -p $(@D)
+	$Q $< > $@~
+	$Q if cmp $@ $@~ 2> /dev/null >&2; then rm $@~; else mv $@~ $@; fi
+
+$(TMPDIR)/gen.mk: $(TOOLSDIR)/gen_mk.sh $(TMPDIR)/index Makefile ; $(info $(M) generating subproject rules…)
+	$Q mkdir -p $(@D)
+	$Q $< $(TMPDIR)/index > $@~
+	$Q if cmp $@ $@~ 2> /dev/null >&2; then rm $@~; else mv $@~ $@; fi
+
+include $(TMPDIR)/gen.mk
+
+fmt: ; $(info $(M) reformatting sources…)
+	$Q find . -name '*.go' | xargs -r $(GOFMT) $(GOFMT_FLAGS)
+
+tidy: fmt
+
+generate: ; $(info $(M) running go:generate…)
+	$Q git grep -l '^//go:generate' | sort -uV | xargs -r -n1 $(GO) generate $(GOGENERATE_FLAGS)
+
+$(REVIVE):
+	$Q $(GO) install -v $(REVIVE_INSTALL_URL)
---
+++ internal/tools/gen_mk.sh
@@ -0,0 +1,168 @@
+#!/bin/sh
+
+set -eu
+
+INDEX="$1"
+
+PROJECTS="$(cut -d':' -f1 "$INDEX")"
+COMMANDS="tidy get build test up"
+
+expand() {
+	local prefix="$1" suffix="$2"
+	local x= out=
+	shift 2
+
+	for x; do
+		out="${out:+$out }${prefix}$x${suffix}"
+	done
+
+	echo "$out"
+}
+
+prefixed() {
+	local prefix="${1:+$1-}"
+	shift
+	expand "$prefix" "" "$@"
+}
+
+suffixed() {
+	local suffix="${1:+-$1}"
+	shift
+	expand "" "$suffix" "$@"
+}
+
+gen_install_tools() {
+	cat <<EOT
+for url in \$(GO_INSTALL_URLS); do \$(GO) install -v \$\$url; done
+EOT
+}
+
+gen_revive_exclude() {
+	local self="$1"
+	local dirs= d=
+
+	dirs="$(cut -d: -f2 "$INDEX" | grep -v '^.$' || true)"
+	if [ "." != "$self" ]; then
+		dirs=$(echo "$dirs" | sed -n -e "s;^$self/\(.*\)$;\1;p")
+	fi
+
+	for d in $dirs; do
+		printf -- "-exclude ./$d/... "
+	done
+}
+
+for cmd in $COMMANDS; do
+	all="$(prefixed $cmd $PROJECTS)"
+	depsx=
+
+	cat <<EOT
+.PHONY: $cmd $all
+$cmd: $all
+
+EOT
+
+	# default calls
+	case "$cmd" in
+	tidy)
+		call="$(cat <<EOT | sed -e '/^$/d;'
+\$(GO) mod tidy
+\$(GO) vet ./...
+\$(REVIVE) \$(REVIVE_RUN_ARGS) ./...
+EOT
+)"
+		depsx="fmt \$(REVIVE)"
+		;;
+	up)
+		call="\$(GO) get -u -v ./...
+\$(GO) mod tidy"
+		;;
+	test)
+		call="\$(GO) $cmd ./..."
+		;;
+	*)
+		call="\$(GO) $cmd -v ./..."
+		;;
+	esac
+
+	case "$cmd" in
+	build|test)
+		sequential=true ;;
+	*)
+		sequential=false ;;
+	esac
+
+	while IFS=: read name dir mod deps; do
+
+		deps=$(echo "$deps" | tr ',' ' ')
+
+		# cd $dir
+		if [ "." = "$dir" ]; then
+			# root
+			cd=
+		else
+			cd="cd '$dir' \&\& "
+		fi
+
+		callx="$call"
+		if [ "$name" = root ]; then
+			# special case
+			case "$cmd" in
+			get)
+				cmdx="get -tags tools"
+				;;
+			up)
+				cmdx="get -tags tools -u"
+				;;
+			*)
+				cmdx=
+				;;
+			esac
+
+			[ -z "$cmdx" ] || cmdx="\$(GO) $cmdx -v ./..."
+
+			if [ "up" = "$cmd" ]; then
+				callx="$cmdx
+\$(GO) mod tidy
+$(gen_install_tools)"
+			elif [ "get" = "$cmd" ]; then
+				callx="$cmdx
+$(gen_install_tools)"
+			elif [ -n "$cmdx" ]; then
+				classx="$cmdx"
+			fi
+		fi
+
+		if [ "build" = "$cmd" ]; then
+			# special build flags for cmd/*
+			#
+			callx="if \$(GO) list ./... | grep -e '.*/cmd/[^/]\+\$\$' > /dev/null; then \
+\$(GO_BUILD_CMD) ./...; else \
+\$(GO_BUILD) ./...; fi"
+		fi
+
+		if [ "tidy" = "$cmd" ]; then
+			# exclude submodules when running revive
+			#
+			exclude=$(gen_revive_exclude "$dir")
+			if [ -n "$exclude" ]; then
+				callx=$(echo "$callx" | sed -e "s;\(REVIVE)\);\1 $exclude;")
+			fi
+		fi
+
+		if ! $sequential; then
+			deps=
+		fi
+
+		cat <<EOT
+$cmd-$name:${deps:+ $(prefixed $cmd $deps)}${depsx:+ | $depsx} ; \$(info \$(M) $cmd: $name)
+$(echo "$callx" | sed -e "/^$/d;" -e "s|^|\t\$(Q) $cd|")
+
+EOT
+	done < "$INDEX"
+done
+
+for x in $PROJECTS; do
+	cat <<EOT
+$x: $(suffixed $x get build tidy)
+EOT
+done
